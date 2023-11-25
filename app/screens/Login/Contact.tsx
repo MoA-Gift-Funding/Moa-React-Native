160f00d28d686c9eae3526eebf0f20a4062d359c
greeneeya <suzythetravelerr@gmail.com>
2023-11-25 23:28:27 +0900

    style: 연락처 문구 변경

--- app/screens/Login/Contact.tsx
+++ app/screens/Login/Contact.tsx
@@ -58,27 +58,30 @@ const Contact = ({navigation}) => {
         });
     }
     // 친구 저장 API 호출
-    navigation.navigate('JoinCompleted');
+    navigation.navigate('Birthday');
   };
   return (
     <View className="px-6 bg-white h-full flex flex-col justify-between">
       <View>
         <View className="my-8 font-semibold">
-          <TextSemiBold style="text-Heading-3" title="친구들의 연락처를" />
-          <TextSemiBold style="text-Heading-3" title="연동할까요?" />
+          <TextSemiBold style="text-Heading-3" title="연락처를 연결하면" />
+          <TextSemiBold
+            style="text-Heading-3"
+            title="친구에게 선물할 수 있어요."
+          />
           <TextRegular
             style="text-Body-2 mt-4"
-            title="연락처를 연동하면 친구들의 펀딩을 볼 수 있어요!"
+            title="연락처 연결을 통해 친구들이 원하는 선물을 구경해봐요!"
           />
         </View>
-        <View className="flex flex-col items-start relative mt-2">
+        {/* <View className="flex flex-col items-start relative mt-2">
           <Image
             className="w-[300px] h-[350px] mt-2"
             source={{
               uri: 'https://res.cloudinary.com/dkjk8h8zd/image/upload/v1700910992/contacts_rngrw4.png',
             }}
           />
-        </View>
+        </View> */}
       </View>
       <View className="mb-8 flex flex-col items-center">
         <Pressable onPress={() => navigation.navigate('JoinCompleted')}>
@@ -88,7 +91,7 @@ const Contact = ({navigation}) => {
           />
         </Pressable>
         <NextButton
-          title="연동하기"
+          title="연락처 연결하기"
           handleSubmit={handleSubmit}
           onSubmit={getContacts}
         />
