43871900c8130d32e79d7c3f19a5e3760f1566fe
greeneeya <suzythetravelerr@gmail.com>
2023-11-29 15:15:00 +0900

    feat: authentication router 적용

--- App.tsx
+++ App.tsx
@@ -1,74 +1,17 @@
 import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import Home from './app/screens/Home';
-import Login from './app/screens/Login/Login';
-import BackHeader from './app/components/header/BackHeader';
-import Join from './app/screens/Login/Join';
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 import {UserContextProvider} from './app/contexts/UserContext';
-import PhoneValidation from './app/screens/Login/PhoneValidation';
-import Profile from './app/screens/Login/Profile';
-import Contact from './app/screens/Login/Contact';
-import JoinCompleted from './app/screens/Login/JoinCompleted';
-
-const Stack = createNativeStackNavigator();
+import AuthRouter from './app/router/AuthRouter';
 
 export const App = () => {
   const queryClient = new QueryClient();
+
   return (
     <QueryClientProvider client={queryClient}>
       <UserContextProvider>
         <NavigationContainer>
-          <Stack.Navigator>
-            <Stack.Screen
-              name="Home"
-              component={Home}
-              options={{
-                header: () => <BackHeader />,
-              }}
-            />
-            <Stack.Screen
-              name="Login"
-              component={Login}
-              options={{
-                header: () => <BackHeader />,
-              }}
-            />
-            <Stack.Screen
-              name="Join"
-              component={Join}
-              options={{
-                header: () => <BackHeader />,
-              }}
-            />
-            <Stack.Screen
-              name="PhoneValidation"
-              component={PhoneValidation}
-              options={{
-                header: () => <BackHeader />,
-              }}
-            />
-            <Stack.Screen
-              name="Profile"
-              component={Profile}
-              options={{
-                header: () => <BackHeader />,
-              }}
-            />
-            <Stack.Screen
-              name="Contact"
-              component={Contact}
-              options={{
-                header: () => <BackHeader />,
-              }}
-            />
-            <Stack.Screen
-              name="JoinCompleted"
-              component={JoinCompleted}
-              options={{headerShown: false}}
-            />
-          </Stack.Navigator>
+          <AuthRouter />
         </NavigationContainer>
       </UserContextProvider>
     </QueryClientProvider>
---
+++ app/router/AuthRouter.tsx
@@ -0,0 +1,76 @@
+import React from 'react';
+import {useUserContext} from '../contexts/UserContext';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Home from '../screens/Home';
+import Login from '../screens/Login/Login';
+import BackHeader from '../components/header/BackHeader';
+import PhoneValidation from '../screens/Login/PhoneValidation';
+import Profile from '../screens/Login/Profile';
+import Contact from '../screens/Login/Contact';
+import JoinCompleted from '../screens/Login/JoinCompleted';
+import Join from '../screens/Login/Join';
+
+const AuthRouter = () => {
+  const {
+    userState: {user},
+  } = useUserContext();
+  const Stack = createNativeStackNavigator();
+  return (
+    <Stack.Navigator>
+      {user ? (
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{
+            header: () => <BackHeader />,
+          }}
+        />
+      ) : (
+        <>
+          <Stack.Screen
+            name="Login"
+            component={Login}
+            options={{
+              header: () => <BackHeader />,
+            }}
+          />
+          <Stack.Screen
+            name="Join"
+            component={Join}
+            options={{
+              header: () => <BackHeader />,
+            }}
+          />
+          <Stack.Screen
+            name="PhoneValidation"
+            component={PhoneValidation}
+            options={{
+              header: () => <BackHeader />,
+            }}
+          />
+          <Stack.Screen
+            name="Profile"
+            component={Profile}
+            options={{
+              header: () => <BackHeader />,
+            }}
+          />
+          <Stack.Screen
+            name="Contact"
+            component={Contact}
+            options={{
+              header: () => <BackHeader />,
+            }}
+          />
+          <Stack.Screen
+            name="JoinCompleted"
+            component={JoinCompleted}
+            options={{headerShown: false}}
+          />
+        </>
+      )}
+    </Stack.Navigator>
+  );
+};
+
+export default AuthRouter;
