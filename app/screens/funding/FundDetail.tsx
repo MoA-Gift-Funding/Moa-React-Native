c3470fa525ba421f1e079b66fee3ab97644ba5b0
greeneeya <suzythetravelerr@gmail.com>
2023-12-22 16:33:55 +0900

    style: FundDetail Component

--- app/screens/funding/FundDetail.tsx
+++ app/screens/funding/FundDetail.tsx
@@ -31,6 +31,77 @@ const FundDetail = ({navigation}) => {
         }}
       />
       <FundDesc />
+      <View className="mt-4 bg-white flex flex-col items-center">
+        <View className="w-full flex flex-row justify-center border-b-[1px] border-b-Gray-03">
+          <Pressable
+            className={cls(
+              'w-[156px] h-[48px] flex items-center justify-center',
+              {'border-b-Main-01 border-b-2': description},
+            )}
+            onPress={handleSelection}>
+            <TextSemiBold title="소개글" style="text-Gray-10" />
+          </Pressable>
+          <Pressable
+            className={cls(
+              'w-[156px] h-[48px] flex items-center justify-center',
+              {'border-b-Main-01 border-b-2': message},
+            )}
+            onPress={handleSelection}>
+            <TextSemiBold title="메세지" style="text-Gray-10" />
+          </Pressable>
+        </View>
+        <View className="w-full py-6 mx-6">
+          {description && (
+            <>
+              <TextRegular
+                title={'어쩌고저쩌고'}
+                style="text-Gray-06 text-Body-2 leading-Body-2"
+              />
+              <Pressable
+                className="w-full border-y-[1px] border-Gray-02 flex items-center"
+                onPress={() => setCaution(!caution)}>
+                <View className="w-full h-[60px] flex flex-row justify-between items-center">
+                  <TextSemiBold
+                    title="주의 사항"
+                    style="text-Body-2 text-Gray-10"
+                  />
+                  <FontAwesomeIcon
+                    icon={caution ? faChevronDown : faChevronUp}
+                  />
+                </View>
+                {caution && (
+                  <View className="w-full bg-Gray-02 flex items-center py-4">
+                    <TextRegular
+                      title="어쩌저저쩌저"
+                      style="text-Body-2 text-Gray-06 leading-Body-2"
+                    />
+                  </View>
+                )}
+              </Pressable>
+            </>
+          )}
+          {message && (
+            <TextRegular
+              title="ㅗㅑ"
+              style="text-Gray-06 text-Body-2 leading-Body-2"
+            />
+          )}
+        </View>
+      </View>
+      <View className="bg-white flex flex-row items-center py-6 px-2 justify-evenly">
+        <Pressable className="bg-Gray-08 w-[70px] h-[56px] flex items-center justify-center rounded-lg">
+          <TextSemiBold title="공유" style="text-white text-Body-1" />
+        </Pressable>
+        <Pressable
+          className={
+            'h-[56px] w-[234px] bg-Main-01 rounded-lg flex items-center justify-center'
+          }>
+          <TextSemiBold
+            style="text-white text-Body-1 ml-[14px]"
+            title="선물 펀딩하기"
+          />
+        </Pressable>
+      </View>
     </ScrollView>
   );
 };
