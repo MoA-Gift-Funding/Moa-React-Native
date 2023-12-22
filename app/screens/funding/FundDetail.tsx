d32bec643b701bc0c172b4fe71198f23b0991686
greeneeya <suzythetravelerr@gmail.com>
2023-12-22 23:32:06 +0900

    feat: FundComment Component

--- app/screens/funding/FundDetail.tsx
+++ app/screens/funding/FundDetail.tsx
@@ -8,6 +8,7 @@ import {faChevronDown, faChevronUp} from '@fortawesome/free-solid-svg-icons';
 import TextRegular from '../../components/text/TextRegular';
 import {useForm} from 'react-hook-form';
 import FundDesc from './FundDesc';
+import FundComment from './FundComment';
 
 const FundDetail = ({navigation}) => {
   const [loading, setLoading] = useState(false);
@@ -51,16 +52,20 @@ const FundDetail = ({navigation}) => {
         <View className="w-full py-6">
           {description && (
             <>
-              <TextRegular
-                title={'어쩌고저쩌고'}
-                style="text-Gray-06 text-Body-2 leading-Body-2"
-              />
+              <View className="pb-4 mx-6">
+                <TextRegular
+                  title={
+                    '펀딩 상세 소개글할지라도 피는 것이 어디 꽃 현저하게 이것이다. 청춘의 거선의 품었기 것이다. 것은 별과 대중을 피부가 기쁘며, 아름답고 칼이다. 이성은 방지하는 따뜻한 그리하였는가?있으며, 찾아 별과 우리는 무엇을 가진 쓸쓸하랴? 같으며, 불어 거친 어디 그리하였는가? 무엇을 인간에 날카로우나 바이며, 얼음에 만물은 싹이 봄바람이다.붙잡아 얼음과 얼음 것이 착목한는 영원히 위하여서. 꽃이 꽃이 귀는 끝에 것이다.'
+                  }
+                  style="text-Gray-06 text-Body-2 leading-Body-2"
+                />
+              </View>
               <Pressable
                 className="w-full border-y-[1px] border-Gray-02 flex items-center"
                 onPress={() => setCaution(!caution)}>
                 <View className="w-[312px] h-[60px] flex flex-row justify-between items-center">
                   <TextSemiBold
-                    title="주의 사항"
+                    title="펀딩 취소 규정"
                     style="text-Body-2 text-Gray-10"
                   />
                   <FontAwesomeIcon
@@ -70,7 +75,15 @@ const FundDetail = ({navigation}) => {
                 {caution && (
                   <View className="w-full bg-Gray-02 flex items-center py-4">
                     <TextRegular
-                      title="어쩌저저쩌저"
+                      title="- 취소 안돼"
+                      style="text-Body-2 text-Gray-06 w-[312px] leading-Body-2"
+                    />
+                    <TextRegular
+                      title="- 하지마요"
+                      style="text-Body-2 text-Gray-06 w-[312px] leading-Body-2"
+                    />
+                    <TextRegular
+                      title="- 수수료 나와요"
                       style="text-Body-2 text-Gray-06 w-[312px] leading-Body-2"
                     />
                   </View>
@@ -79,10 +92,11 @@ const FundDetail = ({navigation}) => {
             </>
           )}
           {message && (
-            <TextRegular
-              title="ㅗㅑ"
-              style="text-Gray-06 text-Body-2 leading-Body-2"
-            />
+            <View>
+              <FundComment />
+              <FundComment />
+              <FundComment />
+            </View>
           )}
         </View>
       </View>
