5463eaac01a5957adbce93fbb317414d293e5e8f
greeneeya <suzythetravelerr@gmail.com>
2023-12-24 00:23:11 +0900

    feat: Button 금액 추가 기능

--- app/screens/funding/JoinFund.tsx
+++ app/screens/funding/JoinFund.tsx
@@ -1,17 +1,25 @@
 import React from 'react';
-import {KeyboardAvoidingView, Platform, Pressable, View} from 'react-native';
+import {
+  KeyboardAvoidingView,
+  Platform,
+  Pressable,
+  TextInput,
+  View,
+} from 'react-native';
 import TextBold from '../../components/text/TextBold';
 import TextRegular from '../../components/text/TextRegular';
-import TextInputGroup from '../../components/text/TextInputGroup';
-import {useForm} from 'react-hook-form';
+import {Controller, useForm} from 'react-hook-form';
 import NextButton from '../../components/button/NextButton';
 
 const JoinFund = () => {
   const {
     control,
     handleSubmit,
+    setValue,
+    getValues,
     formState: {errors},
   } = useForm();
+  const total = '145000';
   return (
     <KeyboardAvoidingView
       behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
@@ -32,27 +40,60 @@ const JoinFund = () => {
           style="text-Heading-3 leading-Heading-3"
         />
       </View>
-      <View className="mt-14">
-        <TextInputGroup
-          name="price"
-          label=""
-          control={control}
-          error={errors.price}
-          placeholder="더할 금액 입력"
-          keyboardType="number-pad"
-          rules={{required: '펀딩할 금액을 입력해주세요.'}}
-        />
-        <View className="flex flex-row justify-around mt-1">
-          <Pressable className="w-[61px] h-[42px] flex justify-center items-center bg-Gray-02 rounded-lg">
+      <View className="mt-16">
+        <View>
+          <Controller
+            control={control}
+            rules={{
+              required: '펀딩할 금액을 입력해주세요.',
+              pattern: /\d/,
+            }}
+            render={({field: {onChange, onBlur, value}}) => (
+              <TextInput
+                className="w-[312px] h-[56px] rounded-md px-3 text-Body-1 text-black placeholder:text-[#858585] bg-Gray-02"
+                placeholder="더할 금액 입력"
+                onBlur={onBlur}
+                onChangeText={text => {
+                  onChange(text);
+                }}
+                value={value}
+                keyboardType="number-pad"
+              />
+            )}
+            name="price"
+          />
+          {errors.price && (
+            <TextRegular
+              style="text-red-500 text-sm ml-1"
+              title={errors.price.message}
+            />
+          )}
+        </View>
+        <View className="flex flex-row justify-around mt-2">
+          <Pressable
+            className="w-[61px] h-[42px] flex justify-center items-center bg-Gray-02 rounded-lg"
+            onPress={() =>
+              setValue('price', String(Number(getValues().price) + 5000))
+            }>
             <TextRegular title="+5천원" style="text-Gray-06 text-Body-2" />
           </Pressable>
-          <Pressable className="w-[61px] h-[42px] flex justify-center items-center bg-Gray-02 rounded-lg">
+          <Pressable
+            className="w-[61px] h-[42px] flex justify-center items-center bg-Gray-02 rounded-lg"
+            onPress={() =>
+              setValue('price', String(Number(getValues().price) + 10000))
+            }>
             <TextRegular title="+1만원" style="text-Gray-06 text-Body-2" />
           </Pressable>
-          <Pressable className="w-[61px] h-[42px] flex justify-center items-center bg-Gray-02 rounded-lg">
+          <Pressable
+            className="w-[61px] h-[42px] flex justify-center items-center bg-Gray-02 rounded-lg"
+            onPress={() =>
+              setValue('price', String(Number(getValues().price) + 50000))
+            }>
             <TextRegular title="+5만원" style="text-Gray-06 text-Body-2" />
           </Pressable>
-          <Pressable className="px-2 h-[42px] flex justify-center items-center bg-Gray-02 rounded-lg">
+          <Pressable
+            className="px-2 h-[42px] flex justify-center items-center bg-Gray-02 rounded-lg"
+            onPress={() => setValue('price', total)}>
             <TextRegular
               title="남은 금액 채우기"
               style="text-Gray-06 text-Body-2"
