f104cde3232dbd2d0f7ebe3216dcf43bb705d8fb
greeneeya <suzythetravelerr@gmail.com>
2023-12-23 23:55:14 +0900

    style: JoinComponent UI

--- app/screens/funding/JoinFund.tsx
+++ app/screens/funding/JoinFund.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
-import {View} from 'react-native';
+import {KeyboardAvoidingView, Platform, Pressable, View} from 'react-native';
 import TextBold from '../../components/text/TextBold';
 import TextRegular from '../../components/text/TextRegular';
+import TextInputGroup from '../../components/text/TextInputGroup';
+import {useForm} from 'react-hook-form';
+import NextButton from '../../components/button/NextButton';
 
 const JoinFund = () => {
+  const {
+    control,
+    handleSubmit,
+    formState: {errors},
+  } = useForm();
   return (
-    <View className="h-full bg-white px-8">
+    <KeyboardAvoidingView
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      className="h-full bg-white px-6">
       <View className="mt-4">
         <View className="flex flex-row">
           <TextBold
@@ -22,7 +32,42 @@ const JoinFund = () => {
           style="text-Heading-3 leading-Heading-3"
         />
       </View>
-    </View>
+      <View className="mt-14">
+        <TextInputGroup
+          name="price"
+          label=""
+          control={control}
+          error={errors.price}
+          placeholder="더할 금액 입력"
+          keyboardType="number-pad"
+          rules={{required: '펀딩할 금액을 입력해주세요.'}}
+        />
+        <View className="flex flex-row justify-around mt-1">
+          <Pressable className="w-[61px] h-[42px] flex justify-center items-center bg-Gray-02 rounded-lg">
+            <TextRegular title="+5천원" style="text-Gray-06 text-Body-2" />
+          </Pressable>
+          <Pressable className="w-[61px] h-[42px] flex justify-center items-center bg-Gray-02 rounded-lg">
+            <TextRegular title="+1만원" style="text-Gray-06 text-Body-2" />
+          </Pressable>
+          <Pressable className="w-[61px] h-[42px] flex justify-center items-center bg-Gray-02 rounded-lg">
+            <TextRegular title="+5만원" style="text-Gray-06 text-Body-2" />
+          </Pressable>
+          <Pressable className="px-2 h-[42px] flex justify-center items-center bg-Gray-02 rounded-lg">
+            <TextRegular
+              title="남은 금액 채우기"
+              style="text-Gray-06 text-Body-2"
+            />
+          </Pressable>
+        </View>
+      </View>
+      <KeyboardAvoidingView className="absolute bottom-8 right-6">
+        <NextButton
+          title="다음"
+          handleSubmit={handleSubmit}
+          onSubmit={() => {}}
+        />
+      </KeyboardAvoidingView>
+    </KeyboardAvoidingView>
   );
 };
 
