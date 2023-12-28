cce4958f288755e14466b0eb42c5f4c4ae6763ed
greeneeya <suzythetravelerr@gmail.com>
2023-12-28 23:58:35 +0900

    feat: remove zod

--- app/zod/schema.ts
+++
@@ -1,9 +0,0 @@
-import {z} from 'zod';
-
-export const LoginUser = z.object({
-  nickname: z.string(),
-  birthday: z.string(),
-  birthyear: z.string(),
-  phoneNumber: z.string(),
-  fullBirthday: z.coerce.date(),
-});
