eaca07b312a94850c6874e184997a93a0d3246da
greeneeya <suzythetravelerr@gmail.com>
2024-02-20 00:27:03 +0900

    [MOA-244] Modal 생성

--- app/components/button/ReportButton.tsx
+++ app/components/button/ReportButton.tsx
@@ -1,15 +1,37 @@
 import React, {useState} from 'react';
-import {Image, Modal, Pressable, SafeAreaView, View} from 'react-native';
+import {
+  Button,
+  Image,
+  Modal,
+  Pressable,
+  SafeAreaView,
+  View,
+} from 'react-native';
 import TextSemiBold from '../text/TextSemiBold';
 import TextBold from '../text/TextBold';
 import TextRegular from '../text/TextRegular';
 
+const ReportTextBtn = ({
+  title,
+  onPress,
+}: {
+  title: string;
+  onPress: () => void;
+}) => (
+  <Pressable
+    className="w-[312px] h-[56px] bg-Gray-02 flex justify-center rounded-lg mb-2"
+    onPress={onPress}>
+    <TextSemiBold title={title} style="ml-4 text-Gray-06" />
+  </Pressable>
+);
+
 const ReportButton = () => {
-  const handleReportBtn = () => {};
-  const [onModal, setOnModal] = useState(true);
+  const handleOnModal = () => setOnModal(!onModal);
+  const [onModal, setOnModal] = useState(false);
+  const handleReport = () => {};
   return (
     <>
-      <Pressable onPress={handleReportBtn}>
+      <Pressable onPress={handleOnModal} className="px-2 py-2">
         <Image
           className="w-[4px] h-[16px]"
           source={{
@@ -19,20 +41,49 @@ const ReportButton = () => {
       </Pressable>
       {onModal && (
         <Modal>
-          <SafeAreaView className="flex flex-col items-center">
-            <Pressable className="w-full flex flex-row justify-end px-4 py-2">
-              <TextBold title="X" style="text-Gray-08 text-Heading-3" />
-            </Pressable>
-            <View className="mt-14 flex flex-col items-center">
-              <TextSemiBold
-                title="메세지 신고하기"
-                style="text-Heading-3 leading-Heading-3"
-              />
-              <TextRegular
-                title="문제를 가장 잘 설명하는 옵션을 선택해주세요."
-                style="text-Body-2 leading-Body-2"
-              />
+          <SafeAreaView className="h-full flex flex-col items-center justify-between">
+            <View>
+              <Pressable
+                className="w-full flex flex-row justify-end px-4 py-2"
+                onPress={handleOnModal}>
+                <TextBold title="X" style="text-Gray-08 text-Heading-3" />
+              </Pressable>
+              <View className="mt-10 flex flex-col justify-between">
+                <View className="flex flex-col items-center">
+                  <TextSemiBold
+                    title="메세지 신고하기"
+                    style="text-Heading-3 leading-Heading-3"
+                  />
+                  <TextRegular
+                    title="문제를 가장 잘 설명하는 옵션을 선택해주세요."
+                    style="text-Body-2 leading-Body-2"
+                  />
+                  <View className="flex flex-col mt-6">
+                    <ReportTextBtn
+                      title="스팸"
+                      onPress={() => setOnModal(!onModal)}
+                    />
+                    <ReportTextBtn
+                      title="학대나 괴롭힘"
+                      onPress={() => setOnModal(!onModal)}
+                    />
+                    <ReportTextBtn
+                      title="해로운 허위 정보 및 폭력 미화"
+                      onPress={() => setOnModal(!onModal)}
+                    />
+                    <ReportTextBtn
+                      title="기타"
+                      onPress={() => setOnModal(!onModal)}
+                    />
+                  </View>
+                </View>
+              </View>
             </View>
+            <Pressable
+              className="w-[312px] h-[56px] flex items-center justify-center bg-Gray-08 rounded-lg mb-4"
+              onPress={handleOnModal}>
+              <TextSemiBold title="취소" style="text-Gray-02" />
+            </Pressable>
           </SafeAreaView>
         </Modal>
       )}
