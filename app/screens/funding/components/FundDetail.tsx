0249b03a29c9bf023b2ea892498e2286c833affd
greeneeya <suzythetravelerr@gmail.com>
2024-02-24 02:17:26 +0900

    fix: Funding 환불규정 추가

--- app/screens/funding/components/FundDetail.tsx
+++ app/screens/funding/components/FundDetail.tsx
@@ -140,15 +140,15 @@ const FundDetail = ({navigation, route}) => {
                   {caution && (
                     <View className="w-full bg-Gray-02 flex items-center py-4">
                       <TextRegular
-                        title="- 취소 안돼"
+                        title="- 펀딩 참여자의 결제 취소는 펀딩이 종료되기 전까지, 결제일로부터 7일 이내, 마이페이지 펀딩에서 결제 취소가 가능합니다."
                         style="text-Body-2 text-Gray-06 w-[312px] leading-Body-2"
                       />
                       <TextRegular
-                        title="- 하지마요"
+                        title="- 단, 아래의 경우는 결제 취소가 불가합니다."
                         style="text-Body-2 text-Gray-06 w-[312px] leading-Body-2"
                       />
                       <TextRegular
-                        title="- 수수료 나와요"
+                        title="- 제품을 사용 또는 훼손한 경우, 사은품 누락, 상품 TAG, 보증서, 상품 부자재가 제거 혹은 분실된 경우 밀봉 포장을 개봉했거나 내부 포장재를 훼손 또는 분실한 경우(단, 제품확인을 위한 개봉 제외), 시간이 경과되어 재판매가 어려울 정도로 상품가치가 상실된 경우, 기타 전자상거래 등에서의 소비자보호에 관한 법률이 정하는 청약철회 제한사유에 해당하는 경우 결제 취소가 불가능합니다."
                         style="text-Body-2 text-Gray-06 w-[312px] leading-Body-2"
                       />
                     </View>
