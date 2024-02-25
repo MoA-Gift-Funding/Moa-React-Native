49be275289df3fbcbbb8812f11a4ff839582579c
greeneeya <suzythetravelerr@gmail.com>
2024-02-25 17:06:13 +0900

    [MOA-422] 상품 queries

--- app/hooks/products/useProducts.tsx
+++ app/hooks/products/useProducts.tsx
@@ -1,7 +1,38 @@
 import React from 'react';
-import {useMutation, useQuery} from '@tanstack/react-query';
-import {useProductContext} from '../../apis/products/Products';
+import {useInfiniteQuery, useMutation, useQuery} from '@tanstack/react-query';
+import {Products} from '../../apis/products/Products';
+import {useUserContext} from '../../contexts/UserContext';
 
 export default function useProducts(loadingCallback?: () => void) {
-  return {};
+  const {
+    useApi: {client},
+  } = useUserContext();
+  const products = new Products(client);
+
+  const {
+    data: productsQuery,
+    fetchNextPage: productsFetchNextQuery,
+    refetch: productsRefetchQuery,
+  } = useInfiniteQuery({
+    queryKey: ['products'],
+    queryFn: ({pageParam = 0}) => products.getProducts(pageParam, 14),
+    getNextPageParam: lastPage => {
+      if (lastPage.hasNext) {
+        return lastPage.currentPage + 1;
+      }
+      return undefined;
+    },
+    initialPageParam: 0,
+  });
+
+  const {mutateAsync: productDetailQuery} = useMutation({
+    mutationFn: (productId: number) => products.getProductDetail(productId),
+  });
+
+  return {
+    productsQuery,
+    productsFetchNextQuery,
+    productsRefetchQuery,
+    productDetailQuery,
+  };
 }
